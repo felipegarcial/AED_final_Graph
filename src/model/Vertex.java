6304137ccc33cc88f1cbb27dd50fd28e78787ca1
camilovivas <47890754+camilovivas@users.noreply.github.com>
2020-05-11 21:22:52 -0500

    add prim

--- src/model/Vertex.java
+++ src/model/Vertex.java
@@ -1,11 +1,11 @@
 package model;
 
-public class Vertex <V> {
+public class Vertex <V> implements Comparable<Vertex<V>>{
 	public final static char WHITE = 'w';
 	public final static char BLACK = 'b';
 	public final static char GREY = 'g';
 	private V node;
-	private V precesor;
+	private Vertex<V> predecessor;
 	private int distance;
 	private char color;
 	private int index;
@@ -32,4 +32,26 @@ public class Vertex <V> {
 	public int getIndex() {
 		return index;
 	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	@Override
+	public int compareTo(Vertex<V> o) {
+		return distance-o.getDistance();
+	}
+
+	public Vertex<V> getPredecessor() {
+		return predecessor;
+	}
+
+	public void setPredecessor(Vertex<V> predecessor) {
+		this.predecessor = predecessor;
+	}
+
 }
