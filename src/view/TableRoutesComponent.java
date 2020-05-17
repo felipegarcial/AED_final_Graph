8d16cfbedc34736874cf403aee46978694c92b4d
Felipe Garcia <felipegarcial64@gmail.com>
2020-05-17 12:54:28 -0500

    Adding scrool table info routes

--- src/view/TableRoutesComponent.java
+++ src/view/TableRoutesComponent.java
@@ -9,14 +9,15 @@ public class TableRoutesComponent {
 	private PApplet app;
 	private ArrayList<String> placesDraw;
 	private FontComponent fontC;
-	private int posYInfoTable,posYInfo,posYRectScrool;
+	private int posYInfoTable, posYInfo, posYRectScrool;
+
 	public TableRoutesComponent(PApplet app) {
 		this.app = app;
 		placesDraw = new ArrayList<String>();
 		fontC = new FontComponent(app);
 		posYInfo = 0;
 		posYRectScrool = 0;
-		
+
 	}
 
 	public void draw() {
@@ -26,20 +27,39 @@ public class TableRoutesComponent {
 		app.fill(0);
 		app.textSize(13);
 		app.textAlign(PConstants.CENTER);
+
 		
+		//Drawing scrool bar
 		float y2 = PApplet.map(posYInfoTable, 0, 200, 120, 100);
+		app.rect(870, y2, 5, 30);
 		
-		app.rect(870,y2,5,30);
-		for (int i = 0; i < placesDraw.size(); i++) {
+		
+		//Drawing info table
+		for (int i = 1; i < placesDraw.size(); i++) {
 			String[] place = placesDraw.get(i).split(";");
 			for (int j = 0; j < place.length; j++) {
-				int totalWidthString = j == 0?100:(140*j)+100;
-				fontC.draw(i==0 && j==0 ? "ID":place[j], i==0 ? "montserrat-semibold":"montserrat-light", 13, 50, 50, 50,totalWidthString, (i==0?150:150+(i*30)+posYInfoTable));
+				int totalWidthString = j == 0 ? 100 : (140 * j) + 100;
+				fontC.draw(i == 0 && j == 0 ? "ID" : place[j], i == 0 ? "montserrat-semibold" : "montserrat-light", 13,
+						50, 50, 50, totalWidthString, (i == 0 ? 150 : 150 + (i * 30) + posYInfoTable));
+			}
+		}
+
+		app.fill(245);
+		app.rect(26, 120, 800, 50);
+		
+		//Drawing the title
+		if(placesDraw.size()>0) {
+			String[] placeTitles = placesDraw.get(0).split(";");
+			for (int j = 0; j < placeTitles.length; j++) {
+				int totalWidthString = j == 0 ? 100 : (140 * j) + 100;
+				fontC.draw(j == 0 ? "ID" : placeTitles[j], "montserrat-semibold", 13, 50, 50, 50, totalWidthString, 150);
 			}
 		}
 		
-		posYInfo = placesDraw.size()*30;
+		posYInfo = placesDraw.size() * 30;
 	}
+	
+
 
 	public ArrayList<String> getPlacesDraw() {
 		return placesDraw;
@@ -48,9 +68,11 @@ public class TableRoutesComponent {
 	public void setPlacesDraw(ArrayList<String> placesDraw) {
 		this.placesDraw = placesDraw;
 	}
-	
+
 	public void setPosYInfoTable(int posYInfoTable) {
 		this.posYInfoTable = posYInfoTable;
 	}
+	
+	
 
 }
